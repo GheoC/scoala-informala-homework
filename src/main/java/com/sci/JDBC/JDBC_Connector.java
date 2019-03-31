eb42225c888f325916e0198a4c42765a71c9cbe0
GheoC <gheorghe.coanta@gmail.com>
2019-03-31 21:10:29 +0300

    Implement in java the following methods:
    - Display data for all classes.
    - Display data for a course with specified id.
    - Display data for all classes whose title (when converted to all
    lowercase letters) begins with "intro".
    - Display data for all classes whose dept (when converted to all
    lowercase letters) is "cos" and whose coursenum begins with "3".
    
    considering the following SQL database:
    
    The school database consists of these five tables:
    
    1. classes
    Fields: classid, courseid, days, starttime, endtime, bldg, roomnum
    
    2. courses
    Fields: courseid, area, title, descrip, prereqs
    The courseid field of the courses table is related to the courseid field
    of the classes table.
    
    3. crosslistings
    Fields: courseid, dept, coursenum
    The courseid field of the crosslistings table is related to the courseid
    field of the courses table.
    
    4. coursesprofs
    Fields: courseid, profid
    The courseid field of the coursesprofs table is related to the courseid
    field of the courses table.
    
    5. profs
    Fields: profid, profname
    The profid field of the profs table is related to the profid field of
    the coursesprofs table.

--- src/main/java/com/sci/JDBC/JDBC_Connector.java
+++ src/main/java/com/sci/JDBC/JDBC_Connector.java
@@ -1,6 +1,29 @@
 package com.sci.JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/** Connector class to the database "school"
+ *
+ */
 public class JDBC_Connector {
 
+    public Connection getConnection() throws SQLException {
 
+        DriverManager.setLoginTimeout(60);
+        String url = new StringBuilder()
+                .append("jdbc:")
+                .append("mysql")
+                .append("://")
+                .append("localhost:")
+                .append("3306")
+                .append("/")
+                .append("school")
+                .append("?user=")
+                .append("root")
+                .append("&password=")
+                .append("password").toString();
+        return DriverManager.getConnection(url);
+    }
 }
